438c09eac960feb07d701d1ea62249f99e047ccb
sai <sai.pullabhotla@gmail.com>
2012-05-25 20:13:13 -0500

    Made another silly change.

--- src/main/java/com/myjavaworld/test/App.java
+++ src/main/java/com/myjavaworld/test/App.java
@@ -7,12 +7,14 @@ public class App {
 	/**
 	 * Prints the greeting message to the console.
 	 * 
-	 * @param args command line arguments. 
+	 * @param args
+	 *            command line arguments.
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
 		System.out.println("Current Date/Time is: " + new Date());
 		System.out.println("Good Bye!");
+		System.out.println("****");
 	}
 
 }
